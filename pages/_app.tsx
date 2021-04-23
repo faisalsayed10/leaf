39465a2175146026b41b55f85ce18e913ac72d64
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-23 11:17:14 +0530

    chakra UI setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </UserProvider>
   );
 }
