1a3ab97cb3f5315809f14ba7033f500d0a925afe
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-26 14:41:59 +0530

    helper functions

---
+++ util/helpers.ts
@@ -0,0 +1,13 @@
+export function getRandom(arr: string[], n: number) {
+  let result: string[] = new Array(n),
+    len = arr.length,
+    taken = new Array(len);
+  if (n > len)
+    throw new RangeError("getRandom: more elements taken than available");
+  while (n--) {
+    const x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
