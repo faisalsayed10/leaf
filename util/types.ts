f6c25f09dc88ac385ad85082a0060e09684f8994
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-26 17:33:40 +0530

    types

---
+++ util/types.ts
@@ -0,0 +1,39 @@
+export type Subject = {
+  key: string;
+  name: string;
+  subject_type: string;
+  work_count: number;
+  works: Work[];
+  ebook_count: number;
+};
+
+export type SubjectsData = {
+  name: string;
+  value: Subject
+}
+
+export type Work = {
+  key: string;
+  title: string;
+  edition_count: number;
+  cover_id: number;
+  cover_edition_key: string;
+  subject: string[];
+  ia_collection: string[];
+  lendinglibrary: boolean;
+  printdisabled: boolean;
+  lending_edition: string;
+  lending_identifier: string;
+  authors: Author[];
+  first_publish_year: number|null;
+  ia: string;
+  public_scan: boolean;
+  has_fulltext: boolean;
+  checked_out: boolean;
+  availability: any;
+};
+
+export type Author = {
+  key: string;
+  name: string;
+};
