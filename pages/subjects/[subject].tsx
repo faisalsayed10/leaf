a7e69de5d7ce82d91728c50d4fc3bd1b69074634
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-28 12:52:50 +0530

    ssr for each subjects

--- pages/subjects/[subject].tsx
+++ pages/subjects/[subject].tsx
@@ -1,12 +1,82 @@
+import {
+  Center,
+  Container,
+  List,
+  ListItem,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/layout";
+import Book from "@components/Book";
+import Layout from "@components/Layout";
+import { upperCaseTitle } from "@util/helpers";
+import { SubjectExtended } from "@util/types";
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
-interface Props {}
+interface Props {
+  data: SubjectExtended;
+}
 
-const SubjectPage = (props: Props) => {
-  const router = useRouter();
+const SubjectPage: React.FC<Props> = ({ data }) => {
+  console.log(data);
 
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <title>{upperCaseTitle(data.name)}</title>
+      </Head>
+      <Layout>
+        <Text
+          fontSize="3xl"
+          mt="8"
+          textTransform="uppercase"
+          letterSpacing="wide"
+          fontWeight="500"
+          align="center"
+        >
+          {upperCaseTitle(data.name)} -{" "}
+          <Text as="span" display="inline" fontSize="xl">
+            {data.work_count} books
+          </Text>
+        </Text>
+        <SimpleGrid columns={5} my="8" spacing={6} placeItems="center">
+          {data.works.map((work) => (
+            <Book key={work.key} book={work} />
+          ))}
+        </SimpleGrid>
+        <Container flexDir="column">
+          <Center>
+            <Text as="h1" fontSize="2xl" textTransform="uppercase">
+              Related Authors
+            </Text>
+            <List mt="8">
+              {data.authors?.map((author) => (
+                <Link href={author.key}>
+                  <ListItem cursor="pointer" color="blue.800" key={author.key}>
+                    {author.name} - {author.count} books
+                  </ListItem>
+                </Link>
+              ))}
+            </List>
+          </Center>
+        </Container>
+      </Layout>
+    </>
+  );
 };
 
 export default SubjectPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { data } = await axios.get<SubjectExtended>(
+    `https://openlibrary.org/subjects/${context.params.subject}.json?limit=30&details=true`
+  );
+
+  return {
+    props: { data },
+  };
+};
