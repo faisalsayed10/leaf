96b51d7c12476356dced21b75cb6af5489c10914
faisalsayed10 <faisal.sayed502@gmail.com>
2021-05-01 17:03:50 +0530

    added active state to sidebar links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import Icon from "@chakra-ui/icon";
 import { Flex, Text } from "@chakra-ui/layout";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { BiBook, BiBookAlt, BiBookContent } from "react-icons/bi";
 import { BsBookmark } from "react-icons/bs";
@@ -10,23 +11,27 @@ import SearchIcon from "./icons/SearchIcon";
 
 interface Props {}
 
-const ButtonProps = {
-  fontSize: "lg",
-  cursor: 'pointer',
-  mb: "1",
-  ml: "2",
-  py: "2",
-  px: "6",
-  _hover: { bgColor: "gray.200", borderRadius: "2xl" },
-  transitionDuration: "100ms",
-};
-
 const IconProps = {
   boxSize: 7,
   mr: "5",
 };
 
 const Sidebar: React.FC<Props> = () => {
+  const router = useRouter();
+  console.log(router.route);
+
+  const ButtonProps = (href: string) => ({
+    fontSize: "lg",
+    cursor: "pointer",
+    mb: "1",
+    ml: "2",
+    py: "2",
+    px: "6",
+    _hover: { bgColor: "gray.200", borderRadius: "2xl" },
+    transitionDuration: "100ms",
+    className: router.route === href ? "active__sidebar" : null,
+  });
+
   return (
     <Flex
       flexDir="column"
@@ -37,45 +42,45 @@ const Sidebar: React.FC<Props> = () => {
       pos="fixed"
     >
       <Link href="/">
-        <Text {...ButtonProps}>
+        <Text {...ButtonProps("/")}>
           <HomeIcon {...IconProps} />
           Home
         </Text>
       </Link>
       <Link href="/search">
-        <Text {...ButtonProps}>
+        <Text {...ButtonProps("/search")}>
           <SearchIcon {...IconProps} />
           Search
         </Text>
       </Link>
       <br />
       <Link href="/subjects">
-        <Text {...ButtonProps}>
+        <Text {...ButtonProps("/subjects")}>
           <Icon as={MdLibraryBooks} {...IconProps} />
           Genres
         </Text>
       </Link>
       <Link href="/random">
-        <Text {...ButtonProps}>
+        <Text {...ButtonProps("/random")}>
           <Icon as={BiBookContent} {...IconProps} />
           Random Book
         </Text>
       </Link>
       <br />
       <Link href="/future">
-        <Text {...ButtonProps}>
+        <Text {...ButtonProps("/future")}>
           <Icon as={BiBookAlt} {...IconProps} />
           Want To Read
         </Text>
       </Link>
       <Link href="/present">
-        <Text {...ButtonProps}>
+        <Text {...ButtonProps("/present")}>
           <Icon as={BsBookmark} {...IconProps} />
           Currently Reading
         </Text>
       </Link>
       <Link href="/past">
-        <Text {...ButtonProps}>
+        <Text {...ButtonProps("/past")}>
           <Icon as={BiBook} {...IconProps} />
           Already Read
         </Text>
