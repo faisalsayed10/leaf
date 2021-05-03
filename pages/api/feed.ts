e90b89f7aa1ca4decd8f0fa9d674b47d19a172ca
faisalsayed10 <faisal.sayed502@gmail.com>
2021-05-03 18:37:30 +0530

    /api/feed

---
+++ pages/api/feed.ts
@@ -0,0 +1,20 @@
+import { GENRES } from "@lib/constants";
+import { getRandom } from "@util/helpers";
+import { FeedData, SearchResponse } from "@util/types";
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+
+// GET /api/feed
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const randomGenres = getRandom(GENRES, 5);
+  const data: FeedData[] = [];
+
+  for (const genre of randomGenres) {
+    const url = `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&key=${process.env.GOOGLE_BOOKS_API_KEY}&maxResults=5`;
+    const response = await axios.get<SearchResponse>(url);
+    data.push({ name: genre, value: response.data });
+  }
+
+  return res.status(200).json(data);
+};
