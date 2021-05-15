2410b8ff427e6374e6bdd0c253a8dc3fdb69553a
faisalsayed10 <faisal.sayed502@gmail.com>
2021-05-15 12:39:31 +0530

    Categories Component

---
+++ components/Categories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  categories: string[]
+}
+
+const Categories: React.FC<Props> = ({ categories }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Categories
