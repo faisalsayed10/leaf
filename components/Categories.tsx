3d4bd486e0d068f83f4ec05d4a38ba2e3a37d8a4
faisalsayed10 <faisal.sayed502@gmail.com>
2021-05-15 17:24:08 +0530

    Show related book categories

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import { Button } from "@chakra-ui/button";
+import { Flex, Text, Wrap, WrapItem } from "@chakra-ui/layout";
+import { readableTitle } from "@util/helpers";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { ImBooks } from "react-icons/im";
 
 interface Props {
-  categories: string[]
+  categories: string[];
 }
 
 const Categories: React.FC<Props> = ({ categories }) => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [uniqueCategories, setUniqueCategories] = useState<string[]>([]);
 
-export default Categories
+  useEffect(() => {
+    const flattened = categories.map((categ) => categ.split(" / ")).flat();
+    const unique = [...new Set(flattened)].map((c) =>
+      c.toLowerCase().replace(/\s/gi, "_")
+    );
+    setUniqueCategories(unique);
+  }, [categories]);
+
+  return (
+    <>
+      <Text fontSize="lg" align="center" fontWeight="600" my="1">
+        Related Genres
+      </Text>
+      <Wrap spacing={3} align="center" justify="start" mx="4">
+        {uniqueCategories.map((genre) => (
+          <Link href={`/genre/${genre}`} key={genre}>
+            <Button
+              as={WrapItem}
+              leftIcon={<ImBooks size={24} />}
+              variant="outline"
+              _hover={{ transform: "scale(1.05)" }}
+              transitionDuration="300ms"
+              cursor="pointer"
+            >
+              {readableTitle(genre)}
+            </Button>
+          </Link>
+        ))}
+      </Wrap>
+    </>
+  );
+};
+
+export default Categories;
