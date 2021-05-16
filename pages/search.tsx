eeba7f8a3b62401498a837c6bf8f9907cb491ff5
faisalsayed10 <faisal.sayed502@gmail.com>
2021-05-16 18:29:21 +0530

    Search page setup

---
+++ pages/search.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/layout";
+import Layout from "@components/Layout";
+
+interface Props {}
+
+const Search: React.FC<Props> = () => {
+  return (
+    <Layout>
+      <Box></Box>
+    </Layout>
+  );
+};
+
+export default Search;
