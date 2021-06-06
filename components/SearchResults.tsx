6284672c07e6bec72763edea2247e538f57970d4
faisalsayed10 <faisal.sayed502@gmail.com>
2021-06-06 15:03:16 +0530

    Search Results component setup

---
+++ components/SearchResults.tsx
@@ -0,0 +1,16 @@
+import { SearchItem } from '@util/types'
+import React from 'react'
+
+interface Props {
+  results?: SearchItem[]
+}
+
+const SearchResults: React.FC<Props> = ({ results }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchResults
