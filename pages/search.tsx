5cbecbd9d2cc256102c044344c5a3acd21560bac
faisalsayed10 <faisal.sayed502@gmail.com>
2021-06-09 10:51:52 +0530

    Remove formik, integrate react-hook-form

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,6 +5,7 @@ import {
   AccordionItem,
   AccordionPanel,
 } from "@chakra-ui/accordion";
+import { Button } from "@chakra-ui/button";
 import { Input } from "@chakra-ui/input";
 import { Box, Container, Flex } from "@chakra-ui/layout";
 import { Select } from "@chakra-ui/select";
@@ -16,6 +17,7 @@ import { SearchResponse } from "@util/types";
 import axios from "axios";
 import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { MdArrowDropDown } from "react-icons/md";
 import _ from "underscore";
 
@@ -43,24 +45,33 @@ interface Props {}
 // &startIndex= - The position in the collection at which to start. The index of the first item is 0.
 // &maxResults= - The maximum number of results to return. The default is 10, and the maximum allowable value is 40.
 
+type Inputs = {
+  author: string;
+  publisher: string;
+  subject: string;
+  isbn: string;
+  filter: string;
+  sort: string;
+};
+
 const Search: React.FC<Props> = () => {
   const [value, setValue] = useState("");
   const [searchData, setSearchData] = useState<SearchResponse>();
-  const authorRef = useRef<HTMLInputElement>();
-  const publisherRef = useRef<HTMLInputElement>();
-  const genreRef = useRef<HTMLInputElement>();
-  const isbnRef = useRef<HTMLInputElement>();
+  const { register, handleSubmit, getValues, formState } = useForm<Inputs>();
+  const { isDirty, dirtyFields, touchedFields } = formState;
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   useEffect(() => {
-    getValue();
+    fetchData();
 
     return () => {
-      getValue.cancel();
+      fetchData.cancel();
     };
   }, [value]);
 
-  const getValue = _.debounce(async () => {
+  const fetchData = _.debounce(async () => {
     if (value.trim() === "") return;
+    const extraOptions = getValues();
     const url = `${BASE_URL}/volumes?q=${encodeURIComponent(value)}`;
 
     const { data } = await axios.post<SearchResponse>("/api/search", { url });
@@ -74,7 +85,7 @@ const Search: React.FC<Props> = () => {
       </Head>
       <Layout>
         <Container my="4" maxW="container.sm">
-          {/* <Box as="form"> */}
+          <Box as="form" onSubmit={handleSubmit(onSubmit)}>
             <SearchInput value={value} setValue={setValue} />
             {/* ____________________ */}
 
@@ -91,21 +102,25 @@ const Search: React.FC<Props> = () => {
                 <AccordionPanel>
                   <Box>
                     <Input
-                      ref={authorRef}
+                      {...register("author")}
                       variant="flushed"
                       placeholder="Author"
                     />
                     <Input
-                      ref={publisherRef}
+                      {...register("publisher")}
                       variant="flushed"
                       placeholder="Publisher"
                     />
                     <Input
-                      ref={genreRef}
+                      {...register("subject")}
                       variant="flushed"
                       placeholder="Genre"
                     />
-                    <Input ref={isbnRef} variant="flushed" placeholder="ISBN" />
+                    <Input
+                      {...register("isbn")}
+                      variant="flushed"
+                      placeholder="ISBN"
+                    />
                   </Box>
                 </AccordionPanel>
               </AccordionItem>
@@ -118,6 +133,7 @@ const Search: React.FC<Props> = () => {
                 icon={<MdArrowDropDown />}
                 placeholder="Filter By:"
                 cursor="pointer"
+                {...register("filter")}
               >
                 <option value="&printType=books">Books</option>
                 <option value="&printType=magazines">Magazines</option>
@@ -126,6 +142,7 @@ const Search: React.FC<Props> = () => {
                 <option value="&filter=paid-ebooks">Paid Ebooks</option>
               </Select>
               <Select
+                {...register("sort")}
                 icon={<MdArrowDropDown />}
                 placeholder="Sort By:"
                 cursor="pointer"
@@ -134,7 +151,8 @@ const Search: React.FC<Props> = () => {
                 <option value="&orderBy=newest">Newest</option>
               </Select>
             </Flex>
-          {/* </Box> */}
+            <Button type="submit">Submit</Button>
+          </Box>
 
           {/* ____________________ */}
           <SearchResults results={searchData && searchData.items} />
