52b86cccc5c8cef104d5d7afa2d1fa17db58e8f4
faisalsayed10 <faisal.sayed502@gmail.com>
2021-06-11 10:56:06 +0530

    Display the fetched data

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,16 +1,37 @@
-import { SearchItem } from '@util/types'
-import React from 'react'
+import { SimpleGrid } from "@chakra-ui/layout";
+import { SearchResponse } from "@util/types";
+import React from "react";
+import Book from "./Book";
 
 interface Props {
-  results?: SearchItem[]
+  results?: SearchResponse;
+  loading: boolean;
 }
 
-const SearchResults: React.FC<Props> = ({ results }) => {
+// Change View - Grid | List
+
+const SearchResults: React.FC<Props> = ({ results, loading }) => {
+  if (loading) return (
+    <SimpleGrid placeItems="center" h="60vh">
+      <p>loading</p>
+    </SimpleGrid>
+  );
+  if ((results && !results.items) || results?.items.length < 1)
+    return (
+      <SimpleGrid placeItems="center" h="60vh">
+        <p>No results found</p>
+      </SimpleGrid>
+    );
+
+  console.log(results);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <SimpleGrid columns={4} spacing={6} placeItems="center" mt="4">
+      {results?.items.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </SimpleGrid>
+  );
+};
 
-export default SearchResults
+export default SearchResults;
