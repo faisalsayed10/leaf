1490794d23f1dde2c4158c50ed0cee2ffd901356
faisalsayed10 <faisal.sayed502@gmail.com>
2021-06-12 13:31:14 +0530

    Switch for grid view and list view

--- pages/testing.tsx
+++ pages/testing.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout";
-import Main from "@components/Main";
-import Sidebar from "@components/Sidebar";
 
-type Props = {};
-
-const Test: React.FC<Props> = (props) => {
-  return (
-    <Layout>
-      {/* <Sidebar /> */}
-    </Layout>
-  );
+const Test: React.FC = () => {
+  return <Layout>a</Layout>;
 };
 
 export default Test;
