de13e287f3240264f89003de5de7d5ce1a74d006
faisalsayed10 <faisal.sayed502@gmail.com>
2021-06-13 12:58:06 +0530

    Create a new swr compatible search api route

--- pages/api/search/[url].ts
+++ pages/api/search/[url].ts
@@ -1,12 +1,16 @@
+import { BASE_URL } from "@lib/constants";
 import { SearchResponse } from "@util/types";
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-// GET /api/search
+// GET /api/search/url
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const url = req.body.url;
+  const url = req.query.url;
   const response = await axios.get<SearchResponse>(
-    url + `&maxResults=40&key=${process.env.GOOGLE_BOOKS_API_KEY}`
+    BASE_URL +
+      "/volumes?" +
+      url +
+      `&maxResults=40&key=${process.env.GOOGLE_BOOKS_API_KEY}`
   );
 
   return res.status(200).json(response.data);
