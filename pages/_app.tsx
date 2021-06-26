875e48eb6527e76d9f71573a0d49edfa7e12d0bf
Faisal <faisal.sayed502@gmail.com>
2021-06-26 12:14:25 +0530

    CSS for react autosuggest elements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,6 +40,60 @@ const GlobalStyle: React.FC = ({ children }) => {
           .readmoreless:hover {
             text-decoration: underline;
           }
+
+          .chakra-collapse {
+            overflow: visible !important;
+          }
+
+          *:focus {
+            box-shadow: none !important;
+          }
+
+          // React Autosuggest Styles
+
+          .react-autosuggest__container {
+            position: relative;
+          }
+
+          .react-autosuggest__suggestions-container {
+            display: none;
+            max-height: 300px;
+            overflow-y: auto;
+          }
+
+          .react-autosuggest__suggestions-container--open {
+            display: block;
+            position: absolute;
+            top: 51px;
+            width: 280px;
+            border: 1px solid #aaa;
+            background-color: #fff;
+            font-family: Helvetica, sans-serif;
+            font-weight: 300;
+            font-size: 16px;
+            border-bottom-left-radius: 4px;
+            border-bottom-right-radius: 4px;
+            z-index: 2;
+          }
+
+          .react-autosuggest__suggestions-list {
+            margin: 0;
+            padding: 0;
+            list-style-type: none;
+          }
+
+          .react-autosuggest__suggestion {
+            cursor: pointer;
+            padding: 10px 20px;
+          }
+
+          .react-autosuggest__suggestion--highlighted {
+            background-color: #ddd;
+          }
+
+          .highlight {
+            font-weight: bold;
+          }
         `}
       />
       {children}
