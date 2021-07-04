972e16b6c0aec71b4352f9b73c399c367c07c0fd
Faisal <faisal.sayed502@gmail.com>
2021-07-04 17:38:26 +0530

    custom verify email page

---
+++ pages/verify.tsx
@@ -0,0 +1,52 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+
+const Verify: React.FC = () => {
+  return (
+    <Flex
+      bg="linear-gradient(to left, #8e9eab, #eef2f3)"
+      w="full"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        flexDir="column"
+        align="stretch"
+        p={8}
+        pb={4}
+        px={10}
+        rounded="lg"
+        shadow="lg"
+        bg="white"
+        w="450px"
+      >
+        <Image
+          src="/libook_logo_white.png"
+          alt="Libook™ logo"
+          maxW="250px"
+          display="block"
+          m="0 auto"
+        />
+        <Text
+          fontWeight="bold"
+          align="center"
+          color="gray.700"
+          mt={8}
+          fontSize="3xl"
+        >
+          Check Your Email
+        </Text>
+        <Text align="center" color="gray.600" fontSize="sm" mb={8}>
+          A sign in link has been sent to your email address
+        </Text>
+        <Text fontWeight="bold" align="center" color="gray.700" mb={8}>
+          <Link href="/">Back to Homepage</Link>
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Verify;
