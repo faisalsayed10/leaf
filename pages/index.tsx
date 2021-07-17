83f72d07ce9b01ca6451663392155191e588c90d
Faisal <faisal.sayed502@gmail.com>
2021-07-17 12:43:29 +0530

    Move homepage to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,46 @@
 import React from "react";
 import Layout from "../components/ui/Layout";
-import Home from "@components/home/Home";
+import GridViewBook from "@components/view-modes/GridViewBook";
 import { useSession } from "next-auth/client";
+import useSWR, { cache } from "swr";
+import { fetcher } from "@lib/fetcher";
+import { Box, Container, Flex, Grid, SimpleGrid } from "@chakra-ui/react";
+import DefaultLoader from "@components/loader/DefaultLoader";
+import { SearchItem } from "@util/types";
 
 type Props = {};
 
 const HomePage: React.FC<Props> = (props) => {
   const [session, loading] = useSession();
+  const { data, error, isValidating } = useSWR<SearchItem[]>(
+    "/api/feed",
+    fetcher,
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      revalidateOnMount: !cache.has("/api/feed"),
+    }
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isValidating)
+    return (
+      <Layout pageTitle="Recommended">
+        <Container my="4" maxW="container.sm">
+          <DefaultLoader />
+        </Container>
+      </Layout>
+    );
 
   return (
-    <Layout>
-      <Home />
+    <Layout pageTitle="Recommended">
+      <SimpleGrid minChildWidth="130px" gap={3} my="8" mx="6" placeItems="center">
+        {data
+          ?.filter((item) => item.volumeInfo.hasOwnProperty("imageLinks"))
+          ?.map((book) => (
+            <GridViewBook key={book.id} book={book} />
+          ))}
+      </SimpleGrid>
     </Layout>
   );
 };
