86daa0672aa6c6873117b481d9740a509e6d3a74
faisalsayed10 <faisal.sayed502@gmail.com>
2021-07-18 15:00:11 +0530

    (feat) show tabbar and hide sidebar for smaller devices

--- components/ui/Layout.tsx
+++ components/ui/Layout.tsx
@@ -1,24 +1,38 @@
 import { Flex } from "@chakra-ui/layout";
 import { Box } from "@chakra-ui/react";
 import React, { ReactNode } from "react";
+import useMedia from "use-media";
 import Navbar from "./Navbar";
 import ProSidebarSection from "./ProSidebar";
-import Sidebar from "./Sidebar";
+import Tabbar from "./Tabbar";
 
 type Props = {
   children: ReactNode;
   pageTitle: string;
 };
 
-const Layout: React.FC<Props> = ({ children, pageTitle }) => (
-  <>
-    <Box height="5px" bgColor="#2ff8bc" pos="sticky" top="0" zIndex="1000" />
-    <Navbar pageTitle={pageTitle} />
-    <Flex bgColor="#edf2f7">
-      <ProSidebarSection />
-      <Box w="100%">{children}</Box>
-    </Flex>
-  </>
-);
+const Layout: React.FC<Props> = ({ children, pageTitle }) => {
+  const isMoreThan400 = useMedia({ minWidth: 400 });
+
+  return (
+    <>
+      <Box height="5px" bgColor="#2ff8bc" pos="sticky" top="0" zIndex="1000" />
+      <Navbar pageTitle={pageTitle} />
+      {isMoreThan400 ? (
+        <Flex bgColor="#edf2f7">
+          <ProSidebarSection />
+          <Box w="100%">{children}</Box>
+        </Flex>
+      ) : (
+        <>
+          <Box w="100%" h="full" bgColor="#edf2f7">
+            {children}
+          </Box>
+          <Tabbar />
+        </>
+      )}
+    </>
+  );
+};
 
 export default Layout;
