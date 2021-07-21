ee122326a1f9c12c364f448ebe48c3261bc375c2
faisalsayed10 <faisal.sayed502@gmail.com>
2021-07-21 16:11:03 +0530

    Add a book to the list api route

---
+++ pages/api/lists/list/[id]/add.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@lib/prisma";
+import { getSession } from "next-auth/client";
+import { VolumeInfo } from "@util/types";
+
+// POST /api/list/:id/add (Add book to a list)
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getSession({ req });
+  const id = req.query.id as string;
+  const book: VolumeInfo & { id: string } = req.body;
+  if (!session) return res.status(401).json({ message: "Unauthorized" });
+
+  const user = await prisma.user.findUnique({
+    where: { email: session.user.email },
+  });
+
+  const list = await prisma.list.findFirst({
+    where: { id, authorId: user.id },
+  });
+
+  if (!list) return res.status(404).json({ message: "List not found" });
+
+  try {
+    await prisma.list.update({
+      where: { id },
+      data: {
+        books: {
+          connectOrCreate: {
+            where: { gbookId: book.id },
+            create: {
+              gbookId: book.id,
+              title: book.title,
+              authors: book.authors,
+              publishedDate: book.publishedDate,
+              previewLink: book.previewLink,
+              imageLinks: Object.values(book.imageLinks),
+            },
+          },
+        },
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({ message: err.message });
+  }
+};
