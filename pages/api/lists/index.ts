4ee7caab57f27b77712dfb3b37acea31e904b04b
faisalsayed10 <faisal.sayed502@gmail.com>
2021-07-21 10:36:17 +0530

    (feat) GET /api/lists

---
+++ pages/api/lists/index.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@lib/prisma";
+import { getSession } from "next-auth/client";
+
+// GET /api/lists
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getSession({ req });
+  if (!session) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: session.user.email },
+    });
+
+    const lists = await prisma.list.findMany({
+      where: { authorId: user.id },
+    });
+
+    return res.status(200).json(lists);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({ message: err.message });
+  }
+};
