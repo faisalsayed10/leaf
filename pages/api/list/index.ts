7b28366e6f29cdfc5c63b3b9833ec4666738ec38
faisalsayed10 <faisal.sayed502@gmail.com>
2021-09-04 16:45:50 +0530

    API changes

--- pages/api/list/index.ts
+++ pages/api/list/index.ts
@@ -1,49 +1,101 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { Book, ListType } from ".prisma/client";
 import prisma from "@lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
 
-// POST /api/list
+interface Body {
+	name: string;
+	description: string;
+	type: ListType;
+	book: Book;
+}
+
+// GET /api/list - Get a list
+// POST /api/list - Create a new list
+// UPDATE /api/list - Update the name/description of a list
+// DELETE /api/list - Delete a list
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
-  if (!session) return res.status(401).json({ message: "Unauthorized" });
-  const { title, description, book } = req.body;
-  if (!title) return res.status(400).json({ message: "Title is required." });
+	const session = await getSession({ req });
+	if (!session) return res.status(401).json({ message: "Unauthorized" });
+	const user = await prisma.user.findUnique({
+		where: { email: session.user.email },
+	});
 
-  try {
-    const user = await prisma.user.findUnique({
-      where: { email: session.user.email },
-    });
+	try {
+		if (req.method === "GET") {
+			const { id, type } = req.query as { id?: string; type?: ListType };
+			const list = await prisma.list.findFirst({
+				where: {
+					...(id && { id }),
+					...(!id && type ? { type } : {}),
+					authorId: user.id,
+				},
+				include: { books: true },
+			});
 
-    const list = await prisma.list.create({
-      data: {
-        title,
-        description: description || "",
-        author: {
-          connect: {
-            id: user.id,
-          },
-        },
-        books: book
-          ? {
-              connectOrCreate: {
-                where: { gbookId: book.id },
-                create: {
-                  gbookId: book.id,
-                  title: book.title,
-                  authors: book.authors,
-                  publishedDate: book.publishedDate,
-                  previewLink: book.previewLink,
-                  imageLinks: Object.values(book.imageLinks),
-                },
-              },
-            }
-          : undefined,
-      },
-    });
+			if (!list) return res.status(404).json({ message: "List not found" });
+			return res.status(200).json(list);
+		} else if (req.method === "POST") {
+			const { name, description, type, book } = req.body as Body;
+			if (!name) return res.status(400).json({ message: "Title is required." });
 
-    return res.status(200).json(list);
-  } catch (err) {
-    console.error(err);
-    return res.status(400).json({ message: err.message });
-  }
+			const user = await prisma.user.findUnique({
+				where: { email: session.user.email },
+			});
+
+			const list = await prisma.list.create({
+				data: {
+					name,
+					description: description || "",
+					type: type || "normal",
+					author: {
+						connect: {
+							id: user.id,
+						},
+					},
+					books: book && {
+						connectOrCreate: {
+							where: { gbookId: book.id },
+							create: {
+								gbookId: book.id,
+								title: book.title,
+								authors: book.authors,
+								publishedDate: book.publishedDate,
+								previewLink: book.previewLink,
+								imageLinks: Object.values(book.imageLinks),
+							},
+						},
+					},
+				},
+			});
+
+			return res.status(200).json(list);
+		} else if (req.method === "PUT") {
+			const { id, type, name, description } = req.body;
+
+			const updatedList = await prisma.list.updateMany({
+				where: {
+					...(id && { id }),
+					...(!id && type ? { type } : {}),
+					authorId: user.id,
+				},
+				data: { name, description },
+			});
+			return res.status(200).json(updatedList);
+		} else if (req.method === "DELETE") {
+			const { id, type } = req.query as { id?: string; type?: ListType };
+
+			await prisma.list.deleteMany({
+				where: {
+					...(id && { id }),
+					...(!id && type ? { type } : {}),
+					authorId: user.id,
+				},
+			});
+			return res.status(200).json({ message: "List deleted" });
+		} else return res.status(405).end();
+	} catch (err) {
+		console.error(err);
+		return res.status(400).json({ message: err.message });
+	}
 };
