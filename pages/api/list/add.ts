b2384b7ebd75dcd09cc4f11ec598b86aede11ffc
faisalsayed10 <faisal.sayed502@gmail.com>
2021-09-04 16:55:40 +0530

    Fixed add and remove api routes

--- pages/api/list/add.ts
+++ pages/api/list/add.ts
@@ -2,48 +2,53 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@lib/prisma";
 import { getSession } from "next-auth/client";
 import { VolumeInfo } from "@util/types";
-// TODO: Fix this route
+import { ListType } from ".prisma/client";
+
 // POST /api/list/add - Add book to a list
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
-  const id = req.query.id as string;
-  const book: VolumeInfo & { id: string } = req.body;
-  if (!session) return res.status(401).json({ message: "Unauthorized" });
+	const session = await getSession({ req });
+	const { id, type } = req.query as { id?: string; type?: ListType };
+	const book: VolumeInfo & { id: string } = req.body;
+	if (!session) return res.status(401).json({ message: "Unauthorized" });
 
-  const user = await prisma.user.findUnique({
-    where: { email: session.user.email },
-  });
+	const user = await prisma.user.findUnique({
+		where: { email: session.user.email },
+	});
 
-  const list = await prisma.list.findFirst({
-    where: { id, authorId: user.id },
-  });
+	const list = await prisma.list.findFirst({
+		where: {
+			...(id && { id }),
+			...(!id && type ? { type } : {}),
+			authorId: user.id,
+		},
+	});
 
-  if (!list) return res.status(404).json({ message: "List not found" });
+	if (!list) return res.status(404).json({ message: "List not found" });
 
-  try {
-    const updatedList = await prisma.list.update({
-      where: { id },
-      data: {
-        books: {
-          connectOrCreate: {
-            where: { gbookId: book.id },
-            create: {
-              gbookId: book.id,
-              title: book.title,
-              authors: book.authors,
-              publishedDate: book.publishedDate,
-              previewLink: book.previewLink,
-              imageLinks: Object.values(book.imageLinks),
-            },
-          },
-        },
-      },
-      include: { books: true },
-    });
+	try {
+		const updatedList = await prisma.list.update({
+			where: { id },
+			data: {
+				books: {
+					connectOrCreate: {
+						where: { gbookId: book.id },
+						create: {
+							gbookId: book.id,
+							title: book.title,
+							authors: book.authors,
+							publishedDate: book.publishedDate,
+							previewLink: book.previewLink,
+							imageLinks: Object.values(book.imageLinks),
+						},
+					},
+				},
+			},
+			include: { books: true },
+		});
 
-    return res.status(200).json(updatedList);
-  } catch (err) {
-    console.error(err);
-    return res.status(400).json({ message: err.message });
-  }
+		return res.status(200).json(updatedList);
+	} catch (err) {
+		console.error(err);
+		return res.status(400).json({ message: err.message });
+	}
 };
--- pages/api/list/remove.ts
+++ pages/api/list/remove.ts
@@ -1,36 +1,41 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@lib/prisma";
 import { getSession } from "next-auth/client";
-// TODO: Fix this route
+import { ListType } from ".prisma/client";
+
 // POST /api/list/remove - Remove book from a list
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
-  const id = req.query.id as string;
-  const gbookId: string = req.body.gbookId;
-  if (!session) return res.status(401).json({ message: "Unauthorized" });
+	const session = await getSession({ req });
+	const { id, type } = req.query as { id?: string; type?: ListType };
+	const gbookId: string = req.body.gbookId;
+	if (!session) return res.status(401).json({ message: "Unauthorized" });
 
-  const user = await prisma.user.findUnique({
-    where: { email: session.user.email },
-  });
+	const user = await prisma.user.findUnique({
+		where: { email: session.user.email },
+	});
 
-  const list = await prisma.list.findFirst({
-    where: { id, authorId: user.id },
-  });
+	const list = await prisma.list.findFirst({
+		where: {
+			...(id && { id }),
+			...(!id && type ? { type } : {}),
+			authorId: user.id,
+		},
+	});
 
-  if (!list) return res.status(404).json({ message: "List not found" });
+	if (!list) return res.status(404).json({ message: "List not found" });
 
-  try {
-    const updatedList = await prisma.list.update({
-      where: { id },
-      data: {
-        books: { disconnect: { gbookId } },
-      },
-      include: { books: true },
-    });
+	try {
+		const updatedList = await prisma.list.update({
+			where: { id },
+			data: {
+				books: { disconnect: { gbookId } },
+			},
+			include: { books: true },
+		});
 
-    return res.status(200).json(updatedList);
-  } catch (err) {
-    console.error(err);
-    return res.status(400).json({ message: err.message });
-  }
+		return res.status(200).json(updatedList);
+	} catch (err) {
+		console.error(err);
+		return res.status(400).json({ message: err.message });
+	}
 };
