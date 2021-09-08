caad0df98130cdf09a3e5b4a5431a7a801641417
faisalsayed10 <faisal.sayed502@gmail.com>
2021-09-08 10:10:24 +0530

    Make grid book and list book to work with prisma Book type

--- components/view-modes/GridViewBook.tsx
+++ components/view-modes/GridViewBook.tsx
@@ -1,68 +1,59 @@
-import { IconButton } from "@chakra-ui/button";
+import { Book } from ".prisma/client";
 import { Image } from "@chakra-ui/image";
 import { Box, Flex, Text } from "@chakra-ui/layout";
-import {
-	Menu,
-	MenuButton,
-	MenuDivider,
-	MenuItem,
-	MenuList,
-} from "@chakra-ui/menu";
+import MoreOptionsMenu from "@components/ui/MoreOptionsMenu";
 import { sliceText } from "@util/helpers";
 import { Item } from "@util/types";
 import Link from "next/link";
 import React from "react";
-import { BiCheck, BiCopy, BiHeart } from "react-icons/bi";
-import { BsBookmark } from "react-icons/bs";
-import { FiMoreVertical } from "react-icons/fi";
-import { MdPlaylistAdd } from "react-icons/md";
 
 interface Props {
-	book: Item;
+	book: Item | Book;
 }
 
-const Book: React.FC<Props> = ({ book }) => {
-	return (
+const GridViewBook: React.FC<Props> = ({ book }) => {
+	return (book as Item).volumeInfo !== undefined ? (
 		<Box maxW="140px" cursor="pointer">
 			<Link href={`/book/${book.id}`}>
 				<Image
-					src={book.volumeInfo?.imageLinks?.thumbnail || ""}
-					alt={sliceText(book.volumeInfo.title, 50)}
+					src={(book as Item).volumeInfo?.imageLinks?.thumbnail || ""}
+					alt={sliceText((book as Item).volumeInfo.title, 50)}
 					fallbackSrc="/no-thumbnail.png"
 					minW="130px"
 					h="180px"
-					m="0 auto"
 				/>
 			</Link>
-			<Flex align="center">
+			<Flex align="center" justifyContent="space-between" w="full">
 				<Link href={`/book/${book.id}`}>
 					<Text isTruncated={true} fontSize="sm" textAlign="center">
-						{book.volumeInfo.title}
+						{(book as Item).volumeInfo.title}
 					</Text>
 				</Link>
-				<Menu>
-					<MenuButton
-						as={IconButton}
-						aria-label="more-options"
-						icon={<FiMoreVertical />}
-					/>
-					<MenuList>
-						<MenuItem icon={<BiHeart size="18" />}>Want To Read</MenuItem>
-						<MenuItem icon={<BsBookmark size="18" />}>
-							Currently Reading
-						</MenuItem>
-						<MenuItem icon={<BiCheck size="18" />}>Already Read</MenuItem>
-						<MenuDivider />
-						<MenuItem icon={<MdPlaylistAdd size="18" />}>
-							Add To Playlist
-						</MenuItem>
-						<MenuDivider />
-						<MenuItem icon={<BiCopy size="18" />}>Copy Link</MenuItem>
-					</MenuList>
-				</Menu>
+				<MoreOptionsMenu />
+			</Flex>
+		</Box>
+	) : (
+		<Box maxW="140px" cursor="pointer">
+			<Link href={`/book/${(book as Book).gbookId}`}>
+				<Image
+					// @ts-ignore
+					src={(book as Book)?.imageLinks?.thumbnail || ""}
+					alt={sliceText((book as Book).title, 50)}
+					fallbackSrc="/no-thumbnail.png"
+					minW="130px"
+					h="180px"
+				/>
+			</Link>
+			<Flex align="center" justifyContent="space-between" w="full">
+				<Link href={`/book/${book.id}`}>
+					<Text isTruncated={true} fontSize="sm" textAlign="left">
+						{(book as Book).title}
+					</Text>
+				</Link>
+				<MoreOptionsMenu />
 			</Flex>
 		</Box>
 	);
 };
 
-export default Book;
+export default GridViewBook;
--- components/view-modes/ListViewBook.tsx
+++ components/view-modes/ListViewBook.tsx
@@ -1,71 +1,82 @@
-import {
-  Flex, Heading, IconButton, Image, Menu,
-  MenuButton, MenuDivider, MenuItem, MenuList, Text
-} from "@chakra-ui/react";
+import { Book } from ".prisma/client";
+import { Flex, Heading, Image, Text } from "@chakra-ui/react";
+import MoreOptionsMenu from "@components/ui/MoreOptionsMenu";
 import { sliceText } from "@util/helpers";
 import { Item } from "@util/types";
 import Link from "next/link";
 import React from "react";
-import { BiCheck, BiCopy, BiHeart } from "react-icons/bi";
-import { BsBookmark } from "react-icons/bs";
-import { FiMoreVertical } from "react-icons/fi";
-import { MdPlaylistAdd } from "react-icons/md";
 
 interface Props {
-  book: Item;
+	book: Item | Book;
 }
 
 const ListViewBook: React.FC<Props> = ({ book }) => {
-  return (
-    <Flex justify="space-between" width="100%" mb="2">
-      <Link href={`/book/${book.id}`}>
-        <Flex cursor="pointer">
-          <Image
-            src={book.volumeInfo?.imageLinks?.thumbnail || ""}
-            alt={sliceText(book.volumeInfo.title, 50)}
-            fallbackSrc="/no-thumbnail.png"
-            minW="145px"
-            maxW="145px"
-            h="200px"
-            mr="6"
-          />
-          <Flex direction="column" mt="3">
-            <Heading as="h4" size="md">
-              {sliceText(book.volumeInfo.title, 50)}
-            </Heading>
-            <Text color="gray.700" fontSize="md">
-              {book.volumeInfo.authors?.length > 0
-                ? sliceText(book.volumeInfo.authors[0], 75)
-                : "Anonymous"}
-            </Text>
-            <Text fontSize="sm" color="gray.500">
-              {book.volumeInfo.description
-                ? sliceText(book.volumeInfo.description, 100)
-                : "No Description Available..."}
-            </Text>
-          </Flex>
-        </Flex>
-      </Link>
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="more-options"
-          icon={<FiMoreVertical />}
-        />
-        <MenuList>
-          <MenuItem icon={<BiHeart size="18" />}>Want To Read</MenuItem>
-          <MenuItem icon={<BsBookmark size="18" />}>Currently Reading</MenuItem>
-          <MenuItem icon={<BiCheck size="18" />}>Already Read</MenuItem>
-          <MenuDivider />
-          <MenuItem icon={<MdPlaylistAdd size="18" />}>
-            Add To Playlist
-          </MenuItem>
-          <MenuDivider />
-          <MenuItem icon={<BiCopy size="18" />}>Copy Link</MenuItem>
-        </MenuList>
-      </Menu>
-    </Flex>
-  );
+	return (book as Item).volumeInfo !== undefined ? (
+		<Flex justify="space-between" width="100%" mb="2">
+			<Link href={`/book/${book.id}`}>
+				<Flex cursor="pointer">
+					<Image
+						src={(book as Item).volumeInfo?.imageLinks?.thumbnail || ""}
+						alt={sliceText((book as Item).volumeInfo.title, 50)}
+						fallbackSrc="/no-thumbnail.png"
+						minW="145px"
+						maxW="145px"
+						h="200px"
+						mr="6"
+					/>
+					<Flex direction="column" mt="3">
+						<Heading as="h4" size="md">
+							{sliceText((book as Item).volumeInfo.title, 50)}
+						</Heading>
+						<Text color="gray.700" fontSize="md">
+							{(book as Item).volumeInfo.authors?.length > 0
+								? sliceText((book as Item).volumeInfo.authors[0], 75)
+								: "Anonymous"}
+						</Text>
+						<Text fontSize="sm" color="gray.500">
+							{(book as Item).volumeInfo.description
+								? sliceText((book as Item).volumeInfo.description, 100)
+								: "No Description Available..."}
+						</Text>
+					</Flex>
+				</Flex>
+			</Link>
+			<MoreOptionsMenu />
+		</Flex>
+	) : (
+		<Flex justify="space-between" width="100%" mb="2">
+			<Link href={`/book/${(book as Book).gbookId}`}>
+				<Flex cursor="pointer">
+					<Image
+						// @ts-ignore
+						src={(book as Book)?.imageLinks?.thumbnail || ""}
+						alt={sliceText((book as Book).title, 50)}
+						fallbackSrc="/no-thumbnail.png"
+						minW="145px"
+						maxW="145px"
+						h="200px"
+						mr="6"
+					/>
+					<Flex direction="column" mt="3">
+						<Heading as="h4" size="md">
+							{sliceText((book as Book).title, 50)}
+						</Heading>
+						<Text color="gray.700" fontSize="md">
+							{(book as Book).authors?.length > 0
+								? sliceText((book as Book).authors[0], 75)
+								: "Anonymous"}
+						</Text>
+						<Text fontSize="sm" color="gray.500">
+							{(book as Book).description
+								? sliceText((book as Book).description, 100)
+								: "No Description Available..."}
+						</Text>
+					</Flex>
+				</Flex>
+			</Link>
+			<MoreOptionsMenu />
+		</Flex>
+	);
 };
 
 export default ListViewBook;
