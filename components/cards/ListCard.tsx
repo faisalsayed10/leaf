f02be393d6b646e411b5d32842c4b74ec3075c79
faisalsayed10 <faisal.sayed502@gmail.com>
2021-10-02 12:08:07 +0530

    List all playlists page

---
+++ components/cards/ListCard.tsx
@@ -0,0 +1,51 @@
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Badge, Box } from "@chakra-ui/layout";
+import CreateListModal from "@components/modals/CreateListModal";
+import { ListWithBooks } from "@util/types";
+import { useRouter } from "next/router";
+import React from "react";
+
+interface Props {
+	list?: ListWithBooks;
+	isGeneric?: true;
+}
+
+const ListCard: React.FC<Props> = ({ list, isGeneric }) => {
+	const router = useRouter();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	return (
+		<Box
+			onClick={() => (isGeneric ? onOpen() : router.push(`/list/${list.id}`))}
+			w="200px"
+			borderWidth="1px"
+			borderRadius="lg"
+			overflow="hidden"
+			bgColor={isGeneric ? "gray.50" : "white"}
+			cursor="pointer">
+			<Box p="4" minH="100">
+				<Box d="flex" alignItems="baseline">
+					<Badge borderRadius="full" px="2" colorScheme="teal">
+						{isGeneric
+							? "Add"
+							: (list?.books?.length || 0) + (list?.books?.length === 1 ? ` book` : ` books`)}
+					</Badge>
+					<Box
+						fontWeight="semibold"
+						as="h4"
+						lineHeight="tight"
+						isTruncated
+						ml="2"
+						textTransform="uppercase">
+						{isGeneric ? "New List" : list.name}
+					</Box>
+				</Box>
+
+				<Box color="gray.500">{isGeneric ? "Create a new list" : list.description}</Box>
+			</Box>
+			<CreateListModal isOpen={isOpen} onClose={onClose} />
+		</Box>
+	);
+};
+
+export default ListCard;
---
+++ pages/lists.tsx
@@ -0,0 +1,66 @@
+import { List } from ".prisma/client";
+import { Container, SimpleGrid, Wrap } from "@chakra-ui/layout";
+import ListCard from "@components/cards/ListCard";
+import DefaultLoader from "@components/loader/DefaultLoader";
+import NoBooksFound from "@components/ui/NoBooksFound";
+import Unauthorized from "@components/ui/Unauthorized";
+import { ListsWithBooks } from "@util/types";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const lists = () => {
+	const [loaded, setLoaded] = useState<boolean>(false);
+	const { data, error, isValidating } = useSWR<ListsWithBooks>(`/api/lists`);
+
+	useEffect(() => {
+		if (loaded) return;
+		if (isValidating) {
+			setLoaded(true);
+		}
+	}, [isValidating]);
+
+	if (isValidating && !data)
+		return (
+			<Container py="4" maxW="container.sm" h="100vh">
+				<DefaultLoader />
+			</Container>
+		);
+
+	if ((data as any)?.message === "Unauthorized")
+		return (
+			<>
+				<Head>
+					<title>Libook — Your Lists</title>
+				</Head>
+				<Unauthorized />
+			</>
+		);
+
+	if (loaded && (!data || data?.length < 1)) return <NoBooksFound />;
+  
+	return (
+		<>
+			<Head>
+				<title>Libook — Your Lists</title>
+			</Head>
+			<Container maxW={["container.sm", "container.sm", "container.lg"]} minH="100vh" py={4} px={8}>
+				<SimpleGrid
+					columns={{ base: 1, sm: 2, md: 3, lg: 4 }}
+					spacingX={{ base: 6, lg: 8 }}
+					spacingY={10}
+					mt={6}
+					placeItems={data?.length > 4 ? "center" : "normal"}
+				>
+					{data?.map((list) => (
+						<ListCard key={list.id} list={list} />
+					))}
+					<ListCard key="add-list" isGeneric={true} />
+				</SimpleGrid>
+			</Container>
+		</>
+	);
+};
+
+export default lists;
