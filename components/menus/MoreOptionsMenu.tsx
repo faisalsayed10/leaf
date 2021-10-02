cde63aa4ed82227b8d3889e89b150396c4886fb2
faisalsayed10 <faisal.sayed502@gmail.com>
2021-10-02 15:38:38 +0530

    Using deploy url primarily in place of localhost

--- components/menus/MoreOptionsMenu.tsx
+++ components/menus/MoreOptionsMenu.tsx
@@ -1,12 +1,6 @@
 import { IconButton } from "@chakra-ui/button";
 import { useDisclosure } from "@chakra-ui/hooks";
-import {
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-} from "@chakra-ui/menu";
+import { Menu, MenuButton, MenuDivider, MenuItem, MenuList } from "@chakra-ui/menu";
 import AuthModal from "@components/modals/AuthModal";
 import CreateListModal from "@components/modals/CreateListModal";
 import { Book, List, ListType } from "@prisma/client";
@@ -26,199 +20,174 @@ import { useSWRConfig } from "swr";
 import ListMenu from "./ListMenu";
 
 interface Props {
-  data: Item | Book;
+	data: Item | Book;
 }
 
 const MoreOptionsMenu: React.FC<Props> = ({ data }) => {
-  const [loading, setLoading] = useState(false);
-  const [session, isSessionLoading] = useSession();
-  const router = useRouter();
-  const [bookData, setBookData] = useState<any>();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const {
-    isOpen: isOpenCreateList,
-    onOpen: onOpenCreateList,
-    onClose: onCloseCreateList,
-  } = useDisclosure();
-  const { mutate } = useSWRConfig();
+	const [loading, setLoading] = useState(false);
+	const [session, isSessionLoading] = useSession();
+	const router = useRouter();
+	const [bookData, setBookData] = useState<any>();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const {
+		isOpen: isOpenCreateList,
+		onOpen: onOpenCreateList,
+		onClose: onCloseCreateList
+	} = useDisclosure();
+	const { mutate } = useSWRConfig();
 
-  useEffect(() => {
-    setBookData({
-      gbookId: (data as Book)?.gbookId || data?.id,
-      title: (data as Book)?.title || (data as Item)?.volumeInfo?.title,
-      description:
-        (data as Book)?.description || (data as Item)?.volumeInfo?.description,
-      authors: (data as Book)?.authors || (data as Item)?.volumeInfo?.authors,
-      publishedDate:
-        (data as Book)?.publishedDate ||
-        (data as Item)?.volumeInfo?.publishedDate,
-      previewLink:
-        (data as Book)?.previewLink || (data as Item)?.volumeInfo?.previewLink,
-      imageLinks:
-        (data as Book)?.imageLinks || (data as Item)?.volumeInfo?.imageLinks,
-    });
-  }, [data]);
+	useEffect(() => {
+		setBookData({
+			gbookId: (data as Book)?.gbookId || data?.id,
+			title: (data as Book)?.title || (data as Item)?.volumeInfo?.title,
+			description: (data as Book)?.description || (data as Item)?.volumeInfo?.description,
+			authors: (data as Book)?.authors || (data as Item)?.volumeInfo?.authors,
+			publishedDate: (data as Book)?.publishedDate || (data as Item)?.volumeInfo?.publishedDate,
+			previewLink: (data as Book)?.previewLink || (data as Item)?.volumeInfo?.previewLink,
+			imageLinks: (data as Book)?.imageLinks || (data as Item)?.volumeInfo?.imageLinks
+		});
+	}, [data]);
 
-  const addBookToList = async (listId?: string, listType?: ListType) => {
-    try {
-      setLoading(true);
-      const res = axios.post<List>("/api/list/add", {
-        listId,
-        listType,
-        ...bookData,
-      });
+	const addBookToList = async (listId?: string, listType?: ListType) => {
+		try {
+			setLoading(true);
+			const res = axios.post<List>("/api/list/add", {
+				listId,
+				listType,
+				...bookData
+			});
 
-      toast.promise(
-        res,
-        {
-          loading: "Adding to your list ⏳",
-          success: ({ data }) => `Added to ${toCapitalizedWords(data.name)}`,
-          error: `Aw man! An error occurred while adding. Maybe, try again later? 😭`,
-        },
-        {
-          style: {
-            minWidth: "250px",
-          },
-        }
-      );
-    } catch (err) {
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  };
+			toast.promise(
+				res,
+				{
+					loading: "Adding to your list ⏳",
+					success: ({ data }) => `Added to ${toCapitalizedWords(data.name)}`,
+					error: `Aw man! An error occurred while adding. Maybe, try again later? 😭`
+				},
+				{
+					style: {
+						minWidth: "250px"
+					}
+				}
+			);
+		} catch (err) {
+			console.error(err);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  const removeBookFromList = async () => {
-    if (!bookData) {
-      console.log("No book data");
-      return;
-    }
+	const removeBookFromList = async () => {
+		if (!bookData) {
+			console.log("No book data");
+			return;
+		}
 
-    const listName = router.asPath.split("/")[2];
-    let listType: ListType;
-    let listId: string;
+		const listName = router.asPath.split("/")[2];
+		let listType: ListType;
+		let listId: string;
 
-    switch (listName) {
-      case "future":
-        listType = "wantToRead";
-        break;
-      case "current":
-        listType = "currentlyReading";
-        break;
-      case "past":
-        listType = "alreadyRead";
-        break;
-      default:
-        listType = "normal";
-        listId = listName;
-    }
+		switch (listName) {
+			case "future":
+				listType = "wantToRead";
+				break;
+			case "current":
+				listType = "currentlyReading";
+				break;
+			case "past":
+				listType = "alreadyRead";
+				break;
+			default:
+				listType = "normal";
+				listId = listName;
+		}
 
-    const key = listId
-      ? `/api/list?id=${listId}`
-      : `/api/list?type=${listType}`;
+		const key = listId ? `/api/list?id=${listId}` : `/api/list?type=${listType}`;
 
-    const { data } = await axios.get<List & { books: Book[] }>(key);
+		const { data } = await axios.get<List & { books: Book[] }>(key);
 
-    mutate(
-      key,
-      data.books.filter((book) => book.gbookId != bookData.gbookId),
-      false
-    );
+		mutate(
+			key,
+			data.books.filter((book) => book.gbookId != bookData.gbookId),
+			false
+		);
 
-    await axios.post<List>("/api/list/remove", {
-      listId,
-      listType,
-      gbookId: bookData.gbookId,
-    });
+		await axios.post<List>("/api/list/remove", {
+			listId,
+			listType,
+			gbookId: bookData.gbookId
+		});
 
-    mutate(key);
+		mutate(key);
 
-    toast.success(`Book removed successfully!`);
-  };
+		toast.success(`Book removed successfully!`);
+	};
 
-  return (
-    <>
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="more-options"
-          icon={<FiMoreVertical />}
-        />
-        <MenuList>
-          <MenuItem
-            icon={<BiHeart size="18" />}
-            onClick={() => {
-              if (isSessionLoading) return;
-              session?.user ? addBookToList(null, "wantToRead") : onOpen();
-            }}
-            disabled={loading}
-          >
-            Want To Read
-          </MenuItem>
-          <MenuItem
-            icon={<BsBookmark size="18" />}
-            onClick={() => {
-              if (isSessionLoading) return;
-              session?.user
-                ? addBookToList(null, "currentlyReading")
-                : onOpen();
-            }}
-            disabled={loading}
-          >
-            Currently Reading
-          </MenuItem>
-          <MenuItem
-            icon={<BiCheck size="18" />}
-            onClick={() => {
-              if (isSessionLoading) return;
-              session?.user ? addBookToList(null, "alreadyRead") : onOpen();
-            }}
-            disabled={loading}
-          >
-            Already Read
-          </MenuItem>
-          <MenuDivider />
-          <ListMenu
-            data={bookData}
-            buttonText="Add To Playlist"
-            onOpen={onOpenCreateList}
-            addBookToList={addBookToList}
-          />
-          <MenuDivider />
-          <MenuItem
-            icon={<BiCopy size="18" />}
-            onClick={() => {
-              copy(
-                `http://localhost:3000/book/${
-                  (data as Book)?.gbookId || data?.id
-                }`
-              );
-              toast("Copied to Clipboard!", { icon: "✅" });
-            }}
-          >
-            Copy Link
-          </MenuItem>
-          {router.route.startsWith("/list") && (
-            <>
-              <MenuDivider />
-              <MenuItem
-                icon={<CgRemoveR size="18" />}
-                onClick={removeBookFromList}
-              >
-                Remove from this List
-              </MenuItem>
-            </>
-          )}
-        </MenuList>
-      </Menu>
-      <AuthModal onClose={onClose} onOpen={onOpen} isOpen={isOpen} />
-      <CreateListModal
-        onClose={onCloseCreateList}
-        isOpen={isOpenCreateList}
-        book={bookData}
-      />
-    </>
-  );
+	return (
+		<>
+			<Menu>
+				<MenuButton as={IconButton} aria-label="more-options" icon={<FiMoreVertical />} />
+				<MenuList>
+					<MenuItem
+						icon={<BiHeart size="18" />}
+						onClick={() => {
+							if (isSessionLoading) return;
+							session?.user ? addBookToList(null, "wantToRead") : onOpen();
+						}}
+						disabled={loading}>
+						Want To Read
+					</MenuItem>
+					<MenuItem
+						icon={<BsBookmark size="18" />}
+						onClick={() => {
+							if (isSessionLoading) return;
+							session?.user ? addBookToList(null, "currentlyReading") : onOpen();
+						}}
+						disabled={loading}>
+						Currently Reading
+					</MenuItem>
+					<MenuItem
+						icon={<BiCheck size="18" />}
+						onClick={() => {
+							if (isSessionLoading) return;
+							session?.user ? addBookToList(null, "alreadyRead") : onOpen();
+						}}
+						disabled={loading}>
+						Already Read
+					</MenuItem>
+					<MenuDivider />
+					<ListMenu
+						data={bookData}
+						buttonText="Add To Playlist"
+						onOpen={onOpenCreateList}
+						addBookToList={addBookToList}
+					/>
+					<MenuDivider />
+					<MenuItem
+						icon={<BiCopy size="18" />}
+						onClick={() => {
+							copy(
+								(process.env.DEPLOY_URL || `http://localhost:3000/`) +
+									`book/${(data as Book)?.gbookId || data?.id}`
+							);
+							toast("Copied to Clipboard!", { icon: "✅" });
+						}}>
+						Copy Link
+					</MenuItem>
+					{router.route.startsWith("/list") && (
+						<>
+							<MenuDivider />
+							<MenuItem icon={<CgRemoveR size="18" />} onClick={removeBookFromList}>
+								Remove from this List
+							</MenuItem>
+						</>
+					)}
+				</MenuList>
+			</Menu>
+			<AuthModal onClose={onClose} onOpen={onOpen} isOpen={isOpen} />
+			<CreateListModal onClose={onCloseCreateList} isOpen={isOpenCreateList} book={bookData} />
+		</>
+	);
 };
 
 export default MoreOptionsMenu;
