4098ddfc766dfc26e0797b2c31179078a026ea90
faisalsayed10 <faisal.sayed502@gmail.com>
2021-10-04 13:47:40 +0530

    Finally fix prisma file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+	var prisma: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
 export default prisma;
